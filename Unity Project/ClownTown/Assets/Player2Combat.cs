406db930e6f644bb9145033da0eaeb245dcbe319
Gregory Pickart <gpsickart88@gmail.com>
2020-04-14 14:35:56 -0400

    Fighting mechanics

---
+++ Unity Project/ClownTown/Assets/Player2Combat.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Combat : MonoBehaviour
+{
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 40;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        //Play attack anim
+        animator.SetTrigger("Attack");
+
+        //Detect enemies in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<HealthBar>().TakeDamage(attackDamage);
+        }
+
+        //Apply damage
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
---
+++ Unity Project/ClownTown/Assets/Player2Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour
+{
+    public Animator animator;
+    public CharacterController2D controller;
+    public float runSpeed = 40;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal2") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+
+        if (Input.GetButtonDown("Jump2"))
+        {
+            jump = true;
+            animator.SetBool("isJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch2"))
+            crouch = true;
+        else if (Input.GetButtonUp("Crouch2"))
+            crouch = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("isCrouching", isCrouching);
+    }
+
+    private void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
