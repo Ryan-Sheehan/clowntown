f135b7b6fca62ace217766ea96f6cc4a9e9ac79e
the-other-ryan-sheehan <ryansheehansotheremail@gmail.com>
2020-04-21 14:24:03 -0400

    character selector

---
+++ Unity Project/ClownTown/Assets/SpriteManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteManager : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public Sprite s;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = transform.Find("Head").GetComponent<SpriteRenderer>();
+        s = Resources.LoadAll<Sprite>("Textures/Heads")[2];
+        //sr.sprite = s;
+        Debug.Log("running");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
