bdcd2ffd619262f29db63f57867b5bb8892535d7
Peter Horvath <peter.horvath@tufts.edu>
2020-04-28 15:20:29 -0400

    can no longer pause in game over

--- Unity Project/ClownTown/Assets/DeathMenu.cs
+++ Unity Project/ClownTown/Assets/DeathMenu.cs
@@ -5,17 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class DeathMenu : MonoBehaviour
 {
-
     public GameObject deathMenuUI;
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-    }
+//    void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.Escape))
+//        {
+//            Application.Quit();
+//        }
+//    }
 
     public void RestartMatch()
     {
--- Unity Project/ClownTown/Assets/PauseMenu.cs
+++ Unity Project/ClownTown/Assets/PauseMenu.cs
@@ -9,11 +9,12 @@ public class PauseMenu : MonoBehaviour
 
     public GameObject pauseMenuUI;
     public GameObject optionsMenuUI;
+    public bool isGameOver;
    
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && !isGameOver)
         {
             if (GameIsPaused)
             {
