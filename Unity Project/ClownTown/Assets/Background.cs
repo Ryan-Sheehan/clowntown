042e4e8ec07d5ae2524c08d24c2079efae7b2758
Gregory Pickart <gpsickart88@gmail.com>
2020-05-07 14:34:19 -0400

    New Stage

---
+++ Unity Project/ClownTown/Assets/Background.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+
+
+    public Texture2D[] frames; 
+    
+    float framesPerSecond = 10.0f;
+
+    void Update() 
+    { 
+        int index = (int) (Time.time * framesPerSecond); 
+        index = index % frames.Length;
+        GetComponent<Renderer>().material.mainTexture = frames[index]; 
+    }
+}
